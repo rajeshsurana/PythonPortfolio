197d44b25e14937dbfc4f24ec3e7a2c0ae2f2d33
Rajesh Surana <rsurana@asu.edu>
2015-05-23 12:15:29 -0700

    Work Break Final
    This program opens a browser with some webpage after every 2 hours to
    remind a user to take a short break from work.

--- WorkBreak/work_break.py
+++ WorkBreak/work_break.py
@@ -1,2 +1,11 @@
+import time
 import webbrowser
-webbrowser.open("www.google.com")
+
+break_count = 0
+total_breaks = 3
+
+print ("This program started at " + time.ctime())
+while break_count < total_breaks:
+    time.sleep(2*60*60)
+    webbrowser.open("www.google.com")
+    break_count += 1
