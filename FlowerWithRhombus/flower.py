e301dc12111a40bd3fa613565846d3acca61dad4
Rajesh Surana <rsurana@asu.edu>
2015-05-23 23:37:28 -0700

    flower with rhombus
    Drawing flower using rhombus rotated repeatedly.

---
+++ FlowerWithRhombus/flower.py
@@ -0,0 +1,25 @@
+import turtle
+
+def draw_rhombus(some_turtle):
+    for i in range(1,3):
+        some_turtle.forward(50)
+        some_turtle.right(30)
+        some_turtle.forward(50)
+        some_turtle.right(150)
+
+
+def draw_flower():
+    window = turtle.Screen()
+    window.bgcolor("white")
+    brad = turtle.Turtle()
+    brad.shape("classic")
+    brad.color("red")
+    brad.speed(0)
+    for i in range(1,73):
+        draw_rhombus(brad)
+        brad.right(5)
+    brad.right(90)
+    brad.forward(200)
+    window.exitonclick()
+
+draw_flower()
