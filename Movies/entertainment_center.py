b7587f861664c9934b1d8626070279c897ab55ba
Rajesh Surana <rsurana@asu.edu>
2015-05-24 22:33:41 -0700

    Showcase for movie Trailers
    This program builds the html page containing posters of movies and on
    click play the trailers.

---
+++ Movies/entertainment_center.py
@@ -0,0 +1,58 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life.",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet.",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+school_of_rock = media.Movie("School of Rock", "Storyline",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "http://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille", "Storyline",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "http://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Storyline",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=BYRWfS2s2v4")
+
+hunger_games = media.Movie("Hunger Games", "Storyline",
+                           "http://upload.wikimedia.org/wikipedia/en/4/4b/Hunger_Games_Film_Poster.jpg",
+                           "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+fresh_tomatoes.open_movies_page(movies)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
