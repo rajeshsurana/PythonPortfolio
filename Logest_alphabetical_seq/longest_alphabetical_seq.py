e43286d1bdd2cc2673547000960a6a55eb9a4dd3
Rajesh Surana <rsurana@asu.edu>
2015-06-15 23:46:48 -0700

    Longest Alphabetical Sequence
    This program takes input as string and returns longest alphabetical
    subsequence.

---
+++ Logest_alphabetical_seq/longest_alphabetical_seq.py
@@ -0,0 +1,20 @@
+def longest_alphabetical_seq(s):
+    """s:String
+       returns longest alphabetical sequence"""
+    low = 0
+    res = ''
+    length = len(s)
+    iterator = 0
+    while iterator + 1 < length:
+        if s[iterator] > s[iterator + 1]:
+            if len(s[low:iterator+1]) > len(res):
+                res = s[low:iterator+1]
+            low = iterator + 1
+        iterator += 1
+    if iterator + 1 == length:
+        if len(s[low:iterator+1]) > len(res):
+            res = s[low:iterator+1]
+    
+    return res
+
+print longest_alphabetical_seq(raw_input('Input String to find longest alphabetical sequence:'))
