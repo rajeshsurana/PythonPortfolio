2985148a07a41bd2617506dc1f18953013ef4853
Rajesh Surana <rsurana@asu.edu>
2015-07-19 19:44:42 -0700

    test and result added

--- Enryption/ps6_encryption.py
+++ Enryption/ps6_encryption.py
@@ -196,9 +196,12 @@ def decryptStory():
 
 if __name__ == '__main__':
     # To test findBestShift:
-    wordList = loadWords()
-    s = applyShift('Hello, world!', 8)
-    bestShift = findBestShift(wordList, s)
+    #wordList = loadWords()
+    #s = applyShift('Hello, world!', 8)
+    #bestShift = findBestShift(wordList, s)
     #assert applyShift(s, bestShift) == 'Hello, world!'
     # To test decryptStory, comment the above four lines and uncomment this line:
-    #    decryptStory()
+    print "********Cipher Text in story.txt*******"
+    print getStoryString()
+    print "********Plain Text after decryption*******"
+    print decryptStory()
